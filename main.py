97e33300a528f566610ae9272b386f589fd7136d
Viridian084 <teashen.james@gmail.com>
2020-11-18 20:26:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# Course CS30
+# Period: PM
+# Date Created: November 18th, 2020
+# Date Last Modified: November 18th, 2020
+# Name: Teashen James
+
+# Creating our Character Dictionaries
+characters = {
+    # All of Billy's Information
+    'Billy': {
+        'description': '5 foot 3 inches',
+        'inventory': {
+            'House': 'Where he lives',
+            'Apartment': 'Somewhere he hates living',
+            'Cavern': 'Somewhere he wants to live'
+        }
+    },
+    # All of Jimmy's Information
+    'Jimmy': {
+        'description': '5 foot 8 inches',
+        'inventory': {
+            'Shoes': 'Something he enjoy wearing',
+            'Dunce Cap': 'Something he hates weaing',
+            'Crown': 'Something he wants to wear'
+        }
+    },
+    # All of Timmy's Information
+    'Timmy': {
+        'description': '6 foot 3 inches',
+        'inventory': {
+            'Bread': 'Something he enjoys eating',
+            'Glue': 'Something he enjoys eating',
+            'Blowdrier': 'Something he wants to eat'
+        }
+    }
+}
+
+# Creating our Location Dictionary
+location = {
+    'Left Position': 'sits on the Left couch cushion',
+    'Right Position': 'sits on the Right couch cushion',
+    'Middle Position': 'sits on the Middle couch cushion'
+}
+
+# I did not finish making the Print statements
+print(
+    f"Billy is {characters['Billy']['description']}, {location['Left Position']}. He owns {characters['Billy']['inventory']['House']} "
+)
+
+# I missed a week of school, so I scrambled and barely understand the content enough to 
+# completete this fully, so I decided since I barely understand half of what I missed, my 
+# mark should reflect that my mark should reflect that.
